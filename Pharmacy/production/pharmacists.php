3cb8fa826f0b93951eec16ed7916747034ffbbb1
Ganesh <prathinaganesh@gmail.com>
2017-03-04 18:13:17 +0530

    pharmacists description

--- Pharmacy/production/pharmacists.php
+++ Pharmacy/production/pharmacists.php
@@ -10,12 +10,91 @@ require_once('header.php');
              <div class="col-md-12 col-sm-12 col-xs-12">
               <div class="x_panel">
                 <div class="x_title">
-                  <h2>Search Medicine <small>By name</small></h2>
+                  <h2>Pharmacists</h2>
                  
                   <div class="clearfix"></div>
                 </div>
                 <div class="x_content">
-                 
+						<div class="row">
+            <div class="col-md-12 col-sm-12 col-xs-12">
+              <div class="x_panel">
+                <div class="x_title">
+                  <h2>List of pharmacists <small>Description</small></h2>
+                  <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                   
+                  </ul>
+                  <div class="clearfix"></div>
+                </div>
+                <div class="x_content">
+                  <div class="dashboard-widget-content">
+
+                    <ul class="list-unstyled timeline widget">
+                      <li>
+                        <div class="block">
+                          <div class="block_content">
+                            <h2 class="title">
+                                              <a>Jane Smith</a>
+                                          </h2>
+                            <div class="byline">
+                              <span>from Pondicherry</span>
+                            </div>
+                            <p class="excerpt">About him
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="block">
+                          <div class="block_content">
+                            <h2 class="title">
+                                              <a>Jane Smith</a>
+                                          </h2>
+                            <div class="byline">
+                              <span>from Pondicherry</span>
+                            </div>
+                            <p class="excerpt">About him
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="block">
+                          <div class="block_content">
+                            <h2 class="title">
+                                              <a>Jane Smith</a>
+                                          </h2>
+                            <div class="byline">
+                              <span>from Pondicherry</span>
+                            </div>
+                            <p class="excerpt">About him
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                      <li>
+                        <div class="block">
+                          <div class="block_content">
+                            <h2 class="title">
+                                              <a>Jane Smith</a>
+                                          </h2>
+                            <div class="byline">
+                              <span>from Pondicherry</span>
+                            </div>
+                            <p class="excerpt">About him
+                            </p>
+                          </div>
+                        </div>
+                      </li>
+                     
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+			</div>
                 </div>
               </div>
             </div>
